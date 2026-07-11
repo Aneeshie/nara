3f96a577037a7f141537efd5f150ab9e0e63e2be
Aneeshie <aneeshdas556@gmail.com>
2026-07-11 14:48:00 +0530

    feat: implement first tauri ipc command

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
+import { invoke } from "@tauri-apps/api/core";
+import { useState } from "react";
 
 function App() {
+  const [helloString, setHelloString] = useState("")
+
+  const handleOnClick = async () => {
+    const hello = await invoke<string>("say_hello")
+    setHelloString(hello)
+  }
+
   return (
     <main>
       <h1>nara</h1>
+      <button onClick={handleOnClick}>say hello</button>
+      <h1>{helloString}</h1>
     </main>
   )
 }
