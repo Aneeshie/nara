9d230174087d33f63c12d054b0a6d237b7239283
Aneeshie <aneeshdas556@gmail.com>
2026-07-11 14:10:52 +0530

    fix: restore docs and flake.nix

---
+++ flake.nix
@@ -0,0 +1,45 @@
+{
+  description = "A reproducible Rust development environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            rustc
+            cargo
+            rustfmt
+            clippy
+            rust-analyzer
+
+            nodejs
+            pnpm
+
+            pkg-config
+
+            cargo-tauri
+          ];
+
+          # Libraries required for building many common Rust crates
+          nativeBuildInputs = with pkgs; [
+            pkg-config
+            openssl
+          ];
+
+          shellHook = ''
+            export PKG_CONFIG_PATH="${pkgs.openssl.dev}/lib/pkgconfig:$PKG_CONFIG_PATH"
+            echo "🦀 Rust development environment loaded!"
+            rustc --version
+          '';
+        };
+      });
+}
+
